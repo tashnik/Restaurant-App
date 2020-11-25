97906df7f5eec8b66a523665220ec8349dbc5880
David Potashnik <davidpotashnik@yahoo.com>
2020-11-25 15:30:52 -0500

    Added food detail view

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -10,17 +10,30 @@ import SwiftUI
 struct AppetizerListView: View {
   
   @StateObject private var viewModel = AppetizerListViewModel()
+  @State private var isShowingDetail = false
+  @State private var selectedAppetizer: Appetizer?
   
   var body: some View {
     ZStack {
       NavigationView {
         List(viewModel.appetizers) { appetizer in
           AppetizerListCell(appetizer: appetizer)
+            .onTapGesture {
+              selectedAppetizer = appetizer
+              isShowingDetail = true
+            }
         }
         .navigationTitle("🍟 Appetizers")
+        .disabled(isShowingDetail)
       }
       .onAppear {
         viewModel.getAppetizers()
+      }
+      .blur(radius: isShowingDetail ? 20 : 0)
+      
+      
+      if isShowingDetail {
+        AppetizerDetailView(appetizer: selectedAppetizer!, isShowingDetail: $isShowingDetail)
       }
       
       if viewModel.isLoading {
